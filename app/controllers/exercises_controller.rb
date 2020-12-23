fd057cdbec40463041d5dfdacab720a27da5fca4
Varshit Dusad <dusadv@gmail.com>
2020-12-23 22:27:51 +0530

    Testing few ideas

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,4 +2,8 @@ class ExercisesController < ApplicationController
   def index
   	@articles = Exercise.all
   end
+
+  def show
+  	@articles = Exercise.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	root "articles#index"
 	get "/exercises", to: "exercises#index"
+	get "/exercises/:id", to: "exercises#show"
 	get "/exercises_more", to: "exercises#index2"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
