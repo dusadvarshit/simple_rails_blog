221e53411fe3163a7cf4c72d44417d9659320279
dusadvarshit <dusadv@gmail.com>
2020-12-23 13:09:46 +0000

    Rolled back Exercise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_100846) do
+ActiveRecord::Schema.define(version: 2020_12_20_164256) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "exercises", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
