209751bfab9827ed69814fbe7e2487f2ce32f7e8
Varshit Dusad <varshitdusad@Varshits-MacBook-Air.local>
2021-01-13 19:23:00 +0530

    Shifting to new macbook

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,9 +1,29 @@
 class ExercisesController < ApplicationController
   def index
-  	@exercises = Exercise.all
+  	@exercise = Exercise.all
   end
 
   def show
-  	@exercises = Exercise.find(params[:id])
+  	@exercise = Exercise.find(params[:id])
   end
+
+  def edit
+  	@exercise = Exercise.find(params[:id])
+  end
+
+  def update
+  	@exercise = Exercise.find(params[:id])
+
+  	if @exercise.update(exercise_params)
+  		redirect_to @exercise
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+  	def exercise_params
+  		params.require(:exercise).permit(:name, :description)
+  	end
+
 end
